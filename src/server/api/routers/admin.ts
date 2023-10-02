3332bb55b2e008a8b775a1c0700c7e2332f57e9c
Christoffer <christoffer@christoffergylin.com>
2023-10-02 19:15:33 +0200

    Created adminRouter

---
+++ src/server/api/routers/admin.ts
@@ -0,0 +1,85 @@
+import { Product } from "@prisma/client";
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const adminRouter = createTRPCRouter({
+
+  getAllProducts: publicProcedure.query(({ ctx }) => {
+    return ctx.db.product.findMany();
+  }),
+
+  createColor: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input, ctx }) => {
+      return ctx.db.color.create({
+        data: {
+          name: input.name
+        }
+      })
+    }),
+
+  createCathegory: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input, ctx }) => {
+      return ctx.db.color.create({
+        data: {
+          name: input.name
+        }
+      })
+    }),
+
+  createProduct: publicProcedure
+    .input(z.object({ name: z.string(), price: z.number(), text: z.string(), discount: z.number().optional(), images: z.string().array().optional(), color: z.string().array(), category: z.string().array() }))
+    .mutation(async ({ input, ctx }) => {
+
+      let discount = null;
+
+      if (input.discount) {
+        discount = input.discount;
+      }
+
+      const connectCategories = input.category.map(catId => ({ id: catId }))
+      const connectColors = input.color.map(colId => ({ id: colId }))
+
+      const newProduct = await ctx.db.product.create({
+        data: {
+          name: input.name,
+          price: input.price,
+          text: input.text,
+          discount: discount,
+          cathegory: {
+            connect: connectCategories,
+          },
+          color: {
+            connect: connectColors,
+          }
+        }
+      })
+
+      if (input.images) {
+
+        const connectImages = input.images.map(imgId => ({ id: imgId }))
+
+        const updatedProduct = await ctx.db.product.update({
+          where: {
+            id: newProduct.id
+          },
+          data: {
+            images: {
+              connect: connectImages,
+            }
+          }
+        })
+
+        return updatedProduct;
+
+      }
+
+      return newProduct
+    }),
+});
