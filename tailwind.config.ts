c40fdca7f6666700d9625eb60505e43321badf6c
risinger7 <viktorrisinger@gmail.com>
2023-10-06 15:02:47 +0200

    landing-page1.1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '192': '48rem',
+        '256': '64rem',
+      },
+      fontFamily: {
+        'poppins': ['"Poppins"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
