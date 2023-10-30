65a63671ca62a2481cd1ed28240394badd454bc3
iida hellström <iidahellstrom@gmail.com>
2023-10-30 12:16:02 +0100

    productid

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,18 +1,47 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
+import ProductCard from "~/component/ProductCard";
 import { api } from "~/utils/api";
 
 const ProductPage = () => {
-
-  const allProducts = api.product.getAllProducts.useQuery().data;
-console.log(allProducts, "products")
   const router = useRouter();
-  const product = api.product.getUnique.useQuery({ id: router.query.id }).data;
-  console.log("product: ", product);
+  const id = router.query.id;
+
+  if (typeof id !== "string") {
+    router.push("/");
+    return;
+  }
+  const product = api.product.getUnique.useQuery({ id }).data;
+  if (!product) {
+    router.push("/");
+    return;
+  }
+
   return (
-    <div className="flex flex-col items-center">
-      <div>{router.query.id}</div>
-      <div>{product ? <div>{product.name}</div> : ""}</div>
-      <div>{product ? <div>{product.price}</div> : ""}</div>
+    <div className="m-6 flex flex-col items-center">
+      <div className="grid grid-cols-1 border border-red-400 md:grid-cols-2 w-full">
+        <div>
+          <Image
+            width={320}
+            height={400}
+            src={"/images/no-image.png"}
+            alt={`${product.name} Product Image`}
+            className="w-full"
+          />
+        </div>
+        <div className="flex flex-col border-2 p-2">
+          <div>{product ? <div>{product.name}</div> : ""}</div>
+          <div>{product ? <div>{product.price}</div> : ""}</div>
+        </div>
+        
+      </div>
+      <div className="border border-red-400 w-full">
+        <h2>Description</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Veritatis soluta, commodi consequatur ipsum itaque fuga voluptatum! 
+          Veritatis libero ipsam veniam. Error fuga eius, molestiae in 
+          nesciunt pariatur facere sit necessitatibus?</p>
+      </div>
     </div>
   );
 };
